62a8ba20a7f0767ef20030f6159b50dd0cc0f93c
Manish Devrani <manishdevrani777@gmail.com>
2023-08-16 12:45:34 +0530

    FEAT: added icons

---
+++ src/constants/images.ts
@@ -0,0 +1,6 @@
+const SEARCH_ICON = "/search-icon.svg";
+const FILTER_ICON = "/filter-icon.svg";
+const PREVIOUS_ARROW_ICON = "/previous-arrow-icon.svg";
+const NEXT_ARROW_ICON = "/next-arrow-icon.svg";
+
+export { SEARCH_ICON, FILTER_ICON, PREVIOUS_ARROW_ICON, NEXT_ARROW_ICON };
