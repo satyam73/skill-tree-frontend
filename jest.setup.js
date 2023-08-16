b4109d5531e18e13966a07727ea4510cfc581ad1
Suhas Khobragade <suhaskhobragade19@gmail.com>
2023-08-16 20:58:44 +0530

    fix: remove comment lines from jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,1 @@
-// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
-// Used for __tests__/testing-library.js
-// Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
