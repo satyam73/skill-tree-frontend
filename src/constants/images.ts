ca54c4311487203598934e4ce71dbd1e22284636
Manish Devrani <manishdevrani777@gmail.com>
2023-08-17 19:56:58 +0530

    FEAT: added dummy avatar image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,5 +2,6 @@ const SEARCH_ICON = "/search-icon.svg";
 const FILTER_ICON = "/filter-icon.svg";
 const PREVIOUS_ARROW_ICON = "/previous-arrow-icon.svg";
 const NEXT_ARROW_ICON = "/next-arrow-icon.svg";
+const DUMMY_AVATAR = "/dummy-avatar.svg";
 
-export { SEARCH_ICON, FILTER_ICON, PREVIOUS_ARROW_ICON, NEXT_ARROW_ICON };
+export { SEARCH_ICON, FILTER_ICON, PREVIOUS_ARROW_ICON, NEXT_ARROW_ICON, DUMMY_AVATAR };
