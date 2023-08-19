9a62f8774aab5d72fa63fa4f8d35c75dd3fdf7b3
Manish Devrani <manishdevrani777@gmail.com>
2023-08-19 23:56:58 +0530

    CHORE: added dummy data and image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,5 +3,6 @@ const FILTER_ICON = "/filter-icon.svg";
 const PREVIOUS_ARROW_ICON = "/previous-arrow-icon.svg";
 const NEXT_ARROW_ICON = "/next-arrow-icon.svg";
 const DUMMY_AVATAR = "/dummy-avatar.svg";
+const DUMMY_IMAGE = "/dummy-img.svg";
 
-export { SEARCH_ICON, FILTER_ICON, PREVIOUS_ARROW_ICON, NEXT_ARROW_ICON, DUMMY_AVATAR };
+export { SEARCH_ICON, FILTER_ICON, PREVIOUS_ARROW_ICON, NEXT_ARROW_ICON, DUMMY_AVATAR, DUMMY_IMAGE };
