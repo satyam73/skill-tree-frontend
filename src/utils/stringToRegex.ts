d6aa99d6f02c2b9f2f8660fd0a7fba552ee96277
Suhas Khobragade <suhaskhobragade19@gmail.com>
2023-08-21 23:00:21 +0530

    removed stringToRegex.ts

--- src/utils/stringToRegex.ts
+++
@@ -1,6 +0,0 @@
-export function stringToRegex(input: string): string {
-    // Escape special characters in the input string
-    const escapedInput = input.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-
-    return escapedInput;
-}
