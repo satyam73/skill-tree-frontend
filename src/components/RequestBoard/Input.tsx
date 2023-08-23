fd6a2b201689002d040a4b75053cb50bce43dbcf
Manish Devrani <manishdevrani777@gmail.com>
2023-08-23 23:13:07 +0530

    FEAT: update input component to improve reusability

--- src/components/RequestBoard/Input.tsx
+++ src/components/RequestBoard/Input.tsx
@@ -1,20 +1,97 @@
+import { BUTTON_ROUNDNESS } from "@/constants/button";
+import { ReactNode } from "react";
+
 type InputProps = {
-    placeholder: string;
-    type: string;
+    label: string;
+    value: string;
+    error?: boolean;
+    placeholder?: string;
+    icon?: ReactNode;
+    disabled?: boolean;
+    name?: string;
+    roundness?: string;
+    variant?: string;
+    iconPosition?: "left" | "right";
+    type: "text" | "number" | "email" | "password";
 };
 
-export default function Input({ placeholder, type }: InputProps) {
+type InputStylesProps = {
+    roundness?: string;
+    variant?: string;
+};
+
+function getInputStyle({ roundness, variant }: InputStylesProps) {
+    const baseStyles = "flex gap-4 py-3 px-5 w-max text-base items-center relative";
+    const variantsStyles = {
+        roundness: {
+            square: "rounded-none",
+            medium: "rounded-xl",
+            pill: "rounded-[50px]",
+        },
+        variant: {
+            filled: "bg-gray-100",
+            outlined: "border-2 border-gray-400 focus-within:border-blue",
+        },
+    };
+
+    const defaultVariants = {
+        roundness: variantsStyles.roundness.pill,
+        variant: variantsStyles.variant.outlined,
+    };
+
+    if (roundness) {
+        if (roundness === BUTTON_ROUNDNESS.PILL) {
+            defaultVariants.roundness = variantsStyles.roundness.pill;
+        } else if (roundness === BUTTON_ROUNDNESS.SQUARE) {
+            defaultVariants.roundness = variantsStyles.roundness.square;
+        } else {
+            defaultVariants.roundness = variantsStyles.roundness.medium;
+        }
+    }
+
+    if (variant) {
+        if (variant === "filled") {
+            defaultVariants.variant = variantsStyles.variant.filled;
+        } else {
+            defaultVariants.variant = variantsStyles.variant.outlined;
+        }
+    }
+
+    return `${baseStyles} ${defaultVariants.roundness} ${defaultVariants.variant}`;
+}
+
+export default function Input({
+    type,
+    label,
+    name,
+    value,
+    error = false,
+    placeholder,
+    disabled = false,
+    icon,
+    iconPosition,
+    roundness,
+    variant,
+}: InputProps) {
     return (
-        <div className="text-black-light font-medium">
-            <label htmlFor="search" className="sr-only">
-                {placeholder}
-            </label>
-            <input
-                id="search"
-                type={type}
-                placeholder={placeholder}
-                className="placeholder:text-black-light outline-none bg-transparent"
-            />
+        <div className={getInputStyle({ roundness, variant })}>
+            {iconPosition === "left" && icon}
+            <div className="text-black-light font-medium">
+                <label htmlFor={label} className="sr-only">
+                    {label}
+                </label>
+                <input
+                    id={label}
+                    type={type}
+                    name={name}
+                    value={value}
+                    placeholder={placeholder}
+                    disabled={disabled}
+                    className="placeholder:text-black-light outline-none bg-transparent w-full"
+                />
+            </div>
+            {iconPosition === "right" || !iconPosition ? icon : null}
+            {error && <p className="text-xs absolute error bottom-[-40%] text-red-dark">Required Field</p>}
         </div>
     );
 }
