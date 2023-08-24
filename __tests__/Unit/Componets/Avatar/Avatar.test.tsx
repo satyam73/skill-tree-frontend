2f0914318343b50e9dbe6574fa4fb1b2daa246f4
Manish Devrani <manishdevrani777@gmail.com>
2023-08-24 20:02:46 +0530

    TEST: added tests for avatar component

---
+++ __tests__/Unit/Componets/Avatar/Avatar.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { dummyImg } from "@/constants/image";
+import Avatar from "@/components/Avatar/Avatar";
+
+describe("Avatar Component", function () {
+    it("Should render the image if src is provided", function () {
+        render(<Avatar src={dummyImg} name="Dummy" width={32} height={32} />);
+
+        expect(screen.getByTestId("avatar-img")).toHaveAttribute("src", dummyImg);
+        expect(screen.getByTestId("avatar-img")).toHaveAttribute("alt", "Dummy");
+        expect(screen.getByTestId("avatar-img")).toHaveAttribute("width", "32");
+        expect(screen.getByTestId("avatar-img")).toHaveAttribute("height", "32");
+    });
+
+    it("Should render the name initials if src is false", function () {
+        render(<Avatar name="Manish" width={32} height={32} />);
+
+        expect(screen.queryByTestId("avatar-img")).not.toBeInTheDocument();
+        expect(screen.getByTestId("avatar-name-intials")).toHaveTextContent("M");
+    });
+});
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -17,9 +17,17 @@ export default function Avatar({ src, name, width, height }: AvatarProps) {
     return (
         <div className="rounded-full">
             {src ? (
-                <Image src={src} alt={name} width={width} height={height} style={avatarStyle} />
+                <Image
+                    src={src}
+                    alt={name}
+                    width={width}
+                    height={height}
+                    style={avatarStyle}
+                    data-testid="avatar-img"
+                />
             ) : (
                 <p
+                    data-testid="avatar-name-intials"
                     className="text-sm font-medium rounded-full bg-gray-100 flex items-center justify-center border border-gray-600"
                     style={{ height: `${height}px`, width: `${width}px` }}
                 >
