df3c377deae8784fe8d419f0196af6e6c6de3e9f
Manish Devrani <manishdevrani777@gmail.com>
2023-08-30 17:46:26 +0530

    TEST: added avatar group tests

---
+++ __tests__/Unit/Componets/AvatarGroup/AvatarGroup.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from "@testing-library/react";
+import AvatarGroup from "@/components/AvatarGroup/AvatarGroup";
+
+describe("Avatar Component", function () {
+    it("should not show the remaining avatars text", function () {
+        const votes = [
+            {
+                name: "Prakash",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+            {
+                name: "Manish",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+            {
+                name: "Yash",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+        ];
+        render(<AvatarGroup votes={votes} />);
+
+        expect(screen.queryByTestId("remaining-avatars")).not.toBeInTheDocument();
+    });
+
+    it("should show the remaing avatar text 1", function () {
+        const votes = [
+            {
+                name: "Prakash",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+            {
+                name: "Manish",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+            {
+                name: "Yash",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+            {
+                name: "Mansi",
+                img: "",
+                date: "16/07/2022",
+                reason: "",
+            },
+        ];
+        render(<AvatarGroup votes={votes} />);
+
+        expect(screen.queryByTestId("remaining-avatars")).toHaveTextContent("1");
+    });
+});
