b3275b57d63340e09423e2bd3fbbb0b4f33ec04b
Manish Devrani <manishdevrani777@gmail.com>
2023-08-30 17:24:02 +0530

    STYLE: made header sticky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,12 @@ export default function Home() {
     return (
         <main className="my-20">
             <div className="lg:bg-white rounded-2xl lg:shadow-[0px_0px_6px_rgba(0,0,0,0.04)] w-full lg:w-11/12 max-w-screen-xl mx-auto lg:p-12">
-                <Wrapper>
+                <Wrapper styles="lg:hidden">
+                    <h1 className="text-2xl lg:text-4xl font-semibold mb-2">Request board</h1>
+                </Wrapper>
+                <Wrapper styles="sticky top-0 bg-[#f9fafb] z-[100] py-3 lg:static lg:bg-transparent lg:py-0">
                     <div className="flex gap-4 lg:items-center lg:justify-between flex-col lg:flex-row">
-                        <h1 className="text-2xl lg:text-4xl font-semibold">Request board</h1>
+                        <h1 className="hidden lg:block text-2xl lg:text-4xl font-semibold">Request board</h1>
                         <div className="flex gap-5">
                             <SearchBox />
                             <Button roundness="pill">
@@ -26,7 +29,7 @@ export default function Home() {
                 </Wrapper>
                 <Wrapper>
                     <div className="mt-4 p-4 flex gap-4 justify-end">
-                        <Button roundness="pill" variant="outlined">
+                        <Button roundness="pill" variant="outline">
                             <GrPrevious />
                             <span>Prev</span>
                         </Button>
--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from "react";
 
 type WrapperTypes = {
     children: ReactNode;
+    styles?: string;
 };
 
-export default function Wrapper({ children }: WrapperTypes) {
-    return <div className="px-4 lg:px-0">{children}</div>;
+export default function Wrapper({ children, styles }: WrapperTypes) {
+    return <div className={`px-4 lg:px-0 ${styles}`}>{children}</div>;
 }
