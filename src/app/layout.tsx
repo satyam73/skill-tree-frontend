d19f13cf7a6c6abfff7ddb49d4a50d25c5556d74
bhtibrewal <bhtibrewal@gmail.com>
2023-09-02 19:35:37 +0530

    add react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "@/utils/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+            </body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
 export default function Home() {
+    const { data, isLoading } = useQuery({
+        queryKey: ["endorsements"],
+        queryFn: () => fetch("https://user1693641217292.requestly.dev/endorsements").then((res) => res.json()),
+    });
+    console.log(data);
     return <main></main>;
 }
