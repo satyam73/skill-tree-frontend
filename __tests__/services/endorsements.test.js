a77e5567eb6723ca01d1fb25ee508d47e2e961df
bhtibrewal <bhtibrewal@gmail.com>
2023-11-02 18:27:14 +0530

    remove console.log

--- __tests__/services/endorsements.test.js
+++ __tests__/services/endorsements.test.js
@@ -1,6 +1,6 @@
 const { QueryClient, QueryClientProvider } = require("@tanstack/react-query");
 
-import { useGetEndorsements } from "@/services/endorsements";
+import { useGetEndorsements, usePostEndorsement } from "@/services/endorsements";
 import { renderHook, waitFor } from "@testing-library/react";
 const { server } = require("../../__mocks__/server");
 
@@ -23,5 +23,9 @@ const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{chi
 test("test useGetEndorsements hook", async () => {
     const { result } = renderHook(() => useGetEndorsements(), { wrapper });
     await waitFor(() => result.current.isSuccess);
-    console.log(result.current.data);
+});
+
+test("test useGetEndorsements hook", async () => {
+    const { result } = renderHook(() => usePostEndorsement({ endorsementData: {} }), { wrapper });
+    await waitFor(() => result.current.isSuccess);
 });
