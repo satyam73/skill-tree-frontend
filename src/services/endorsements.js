67db7d8fbcc15e311d1de1dc2c4f7daa9cfd586e
Satyam Bajpai <bajpai1973satyam@gmail.com>
2024-01-13 16:05:14 +0530

    Setup/prod staging env (#3)
    * chore: change backend base url
    
    * fix: change base url object nesting to value only

--- src/services/endorsements.js
+++ src/services/endorsements.js
@@ -3,11 +3,11 @@ import { BASE_URL } from "@/constants/urls";
 export const useGetEndorsements = () =>
     useQuery({
         queryKey: ["endorsements"],
-        queryFn: () => fetch(`${BASE_URL.dev}/endorsements`).then((res) => res.json()),
+        queryFn: () => fetch(`${BASE_URL}/endorsements`).then((res) => res.json()),
     });
 
 export const usePostEndorsement = ({ endorsementData }) =>
     useMutation({
         mutationKey: ["endorsements"],
-        mutationFn: () => fetch(`${BASE_URL.dev}/endorsements`, endorsementData).then((res) => res.json()),
+        mutationFn: () => fetch(`${BASE_URL}/endorsements`, endorsementData).then((res) => res.json()),
     });
