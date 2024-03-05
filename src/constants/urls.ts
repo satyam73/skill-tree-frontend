e599280a9737c8f6f4cbfd6b2442a9a10e55ca45
Satyam Bajpai <bajpai1973satyam@gmail.com>
2024-03-05 13:43:46 +0530

    Update urls.ts

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://skilltree-api.realdevsquad.com/v1' : 'https://staging-skilltree-api.realdevsquad.com/v1';
 export const RDS_BACKEND_URL = process.env.NEXT_PUBLIC_RDS_BACKEND_URL;
